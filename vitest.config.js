075f9780cdffc2301037c062d3d076ffd8d8e55e
Oleg Polyakov <olegpolyakov@outlook.com>
2026-07-13 14:24:25 +0300

    Add test alias resolving

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
     test: {
         // globalSetup: './test/setup.ts',
         // silent: 'passed-only'
+    },
+    resolve: {
+        alias: {
+            '@': './src'
+        }
     }
 });
