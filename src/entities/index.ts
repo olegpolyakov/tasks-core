de5b5edd0ff8e07fb0fa1d47d4bd28bbf8ad625a
Oleg Polyakov <olegpolyakov@outlook.com>
2026-07-20 13:45:40 +0300

    Add project entity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,3 +1,8 @@
+export {
+    default as Project,
+    type ProjectData,
+    type ProjectSectionData
+} from './project.ts';
 export { default as TasksSettings } from './settings.ts';
 export { default as Tag } from './tag.ts';
 export {
---
+++ src/entities/project.ts
@@ -0,0 +1,47 @@
+import {
+    Entity,
+    EntityData
+} from '@olegpolyakov/core';
+
+export type ProjectSectionData = {
+    id: string;
+    name: string;
+    taskIds: string[];
+};
+
+export type ProjectData = {
+    name: string;
+    icon?: string;
+    description?: string;
+    taskIds: string[];
+    sectionIds: string[];
+    sectionData: Record<string, ProjectSectionData>;
+} & EntityData;
+
+export default class Project extends Entity implements ProjectData {
+    readonly name: string;
+    readonly icon?: string;
+    readonly description?: string;
+    readonly taskIds: string[];
+    readonly sectionIds: string[];
+    readonly sectionData: Record<string, ProjectSectionData>;
+
+    constructor({
+        name = '',
+        icon,
+        description,
+        taskIds = [],
+        sectionIds = [],
+        sectionData = {},
+        ...rest
+    }: Partial<ProjectData>) {
+        super(rest);
+
+        this.name = name;
+        this.icon = icon;
+        this.description = description;
+        this.taskIds = taskIds;
+        this.sectionIds = sectionIds;
+        this.sectionData = sectionData;
+    }
+}
